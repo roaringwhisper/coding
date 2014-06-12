64c0aa867a8464d8da9e5703c530fce3e070b4d1
aby@blade <aby@blade.rackspace.com>
2014-06-12 18:07:49 -0500

    added first scripts

---
+++ scripts/printingfun.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print" Hello again."
+print "I like typing this."
+print "This is fun."
+print "Yay! printing."
+print "I love my teddy bear."
+print "I `said` do not touch this."
+
